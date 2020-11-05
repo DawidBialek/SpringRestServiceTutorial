d71b6c4ea197b3b8ed46220cccde2c419d6aa6cc
DawidBialek <dd.bialek@gmail.com>
2020-11-05 22:55:26 +0100

    Add files

---
+++ src/main/java/com/example/SpringRestServiceTutorial/SpringRestServiceTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringRestServiceTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestServiceTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestServiceTutorialApplication.class, args);
+	}
+
+}
